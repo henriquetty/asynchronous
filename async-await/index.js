adbdbfab148499709840e441bc9636b356f38dde
henriquetty <gamercaipira@gmail.com>
2020-10-07 15:45:46 -0300

    added another async await example with .then

--- async-await/index.js
+++ async-await/index.js
@@ -13,3 +13,17 @@ async function requestApi() {
 }
 
 requestApi();
+
+const apiUrl2 = "https://nekos.life/api/v2/img/hug";
+
+async function nekos() {
+  try {
+    return (response = await axios.get(apiUrl2));
+  } catch (error) {
+    return error;
+  }
+}
+
+nekos().then((res) => {
+  console.log(res.data);
+});
